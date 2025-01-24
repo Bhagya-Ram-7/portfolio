cdef5de0bad0c55cbee900a32bfabc869c21fd09
Bhagya Ram <bhagya.sram@gmail.com>
2025-01-24 11:31:32 -0800

    changed url

--- global.js
+++ global.js
@@ -31,7 +31,7 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    url = !ARE_WE_HOME && !url.startsWith('https') ? '../' + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
     
     //let a = document.createElement('a');
