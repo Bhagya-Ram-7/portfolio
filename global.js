4ec9c13abd1f5e4c7af8be62404f2f38f86e5d79
Bhagya Ram <bhagya.sram@gmail.com>
2025-01-24 18:44:23 -0800

    trying to get localStorage working

--- global.js
+++ global.js
@@ -69,11 +69,13 @@ let select = document.querySelector('select');
 if ("colorScheme" in localStorage){
     document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
     select.value = localStorage.colorScheme; 
+    console.log(localStorage); 
 } 
 select.addEventListener('input', function (event) {
     document.documentElement.style.setProperty('color-scheme', event.target.value);
     console.log('color scheme changed to', event.target.value);
     localStorage.colorScheme = event.target.value; 
+    console.log(localStorage); 
 });
 
 
